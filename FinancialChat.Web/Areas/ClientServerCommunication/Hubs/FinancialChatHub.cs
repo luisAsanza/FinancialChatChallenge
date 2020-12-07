f85e4d4376f04f4245a0028ff4381d4ea724ada8
luisAsanza <luisasanza@hotmail.com>
2020-12-06 23:26:55 -0500

    Add project files.

---
+++ FinancialChat.Web/Areas/ClientServerCommunication/Hubs/FinancialChatHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FinancialChat.Web.Areas.ClientServerCommunication.Hubs
+{
+    /// <summary>
+    /// SignalR Hub class used to handle Financial Chat client-server communication.
+    /// </summary>
+    public class FinancialChatHub : Hub
+    {
+        /// <summary>
+        /// Used by a connected client to send a message to all clients.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string user, string message)
+        {
+            //TODO: Store message to DB
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ FinancialChat.Web/Controllers/FinancialChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FinancialChat.Web.Controllers
+{
+    //[Authorize]
+    public class FinancialChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
